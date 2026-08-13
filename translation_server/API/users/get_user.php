d2bfdb54f325b021013db52bb3dd475b526abdc9
demirdjiany <demirdjiany@gmail.com>
2026-08-13 13:22:11 +0300

    add get_user.php

--- translation_server/API/users/get_user.php
+++ translation_server/API/users/get_user.php
@@ -2,6 +2,22 @@
 
     include(__DIR__ . "/../../database/connection.php");
 
-    // Student B: find and return a player when they sign in here.
+    if (isset($_GET["id"])){
+        $id = $_GET["id"];
+    }
+    else{
+        $id = -1;
+        echo json_encode(["success" => false,"message"=> "user not found"]);
+        return;
+    }
+
+    $sql = "SELECT * FROM users WHERE id = ?";
+    $query = $mysql->prepare($sql);
+    $query->bind_param("i", $id);
+    $query->execute();
+    $result = $query->get_result();
+    $data = $result->fetch_assoc();
+
+    echo json_encode(["success"=> true,"data"=> $data]);
 
 ?>
