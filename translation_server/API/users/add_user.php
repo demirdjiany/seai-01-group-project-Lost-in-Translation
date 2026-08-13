f196553fdbb903294cbdad802f7b9b13132ea069
demirdjiany <demirdjiany@gmail.com>
2026-08-13 13:32:31 +0300

    add add_user.php

--- translation_server/API/users/add_user.php
+++ translation_server/API/users/add_user.php
@@ -2,6 +2,27 @@
 
     include(__DIR__ . "/../../database/connection.php");
 
-    // Student B: create a player and return the new player ID here.
+    if (isset($_POST["special_id"])) {
+        $special_id = $_POST["special_id"];
+    }else{
+        $special_id = -1;
+        echo json_encode(["success" => false, "message"=> "user not found"]);
+        return;
+    }
 
+    $first_name = ["Galaxy", "Planet", "Space", "Star"];
+    $last_name = ["Voyager", "Ranger", "Explorer", "Pirate"];
+
+    $f_name = $first_name[rand(0,3)];
+    $l_name = $last_name[rand(0,3)];
+    $number = rand(0, 999999);
+
+    $username = $f_name . "_" . $l_name . "_" . $number;
+
+    $sql = "INSERT INTO users (special_id, username) VALUES (?, ?)";
+    $query = $mysql->prepare($sql);
+    $query->bind_param("is",$special_id, $username);
+    $query->execute();
+
+    echo json_encode(["success"=> true,"message"=> "user added"]);
 ?>
--- translation_server/API/users/get_user.php
+++ translation_server/API/users/get_user.php
@@ -2,18 +2,18 @@
 
     include(__DIR__ . "/../../database/connection.php");
 
-    if (isset($_GET["id"])){
-        $id = $_GET["id"];
+    if (isset($_GET["special_id"])){
+        $special_id = $_GET["special_id"];
     }
     else{
-        $id = -1;
+        $special_id = -1;
         echo json_encode(["success" => false,"message"=> "user not found"]);
         return;
     }
 
-    $sql = "SELECT * FROM users WHERE id = ?";
+    $sql = "SELECT * FROM users WHERE special_id = ?";
     $query = $mysql->prepare($sql);
-    $query->bind_param("i", $id);
+    $query->bind_param("i", $special_id);
     $query->execute();
     $result = $query->get_result();
     $data = $result->fetch_assoc();
